75d38ed61261a8d61a42b308d659f7790ea6e52f
Hyunje Alex Jun <me@noraesae.net>
2015-11-13 13:51:04 +0900

    Remove root project
    Won't be used

--- app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Lib
-
-main :: IO ()
-main = do
-  title <- prompt "Please enter the title of today's work: "
-  putStrLn $ "You did " ++ title
--- src/Lib.hs
+++
@@ -1,9 +0,0 @@
-module Lib where
-
-import System.IO
-
-prompt :: String -> IO String
-prompt text = do
-  putStr text
-  hFlush stdout
-  getLine
