8309f50c7d94d77d3cebfe713a7d43d5f8498f68
Hyunje Alex Jun <me@noraesae.net>
2015-11-17 13:56:31 +0900

    Solve Project Euler #19

--- projecteuler/problem19/app/Main.hs
+++ projecteuler/problem19/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Euler
+
+sunday :: Int -> Int -> Int -> Int
+sunday year month day =
+  if getDayOfWeek year month day == 0
+    then 1
+    else 0
+
+isFirstSunday :: Int -> Int -> Int
+isFirstSunday year month = sunday year month 1
+
+firstSundaysInYear :: Int -> Int
+firstSundaysInYear year = foldl1 (+) (map (isFirstSunday year) [1..12])
 
 main :: IO ()
-main = someFunc
+main =
+  putStrLn $ show $ foldl1 (+) (map firstSundaysInYear [1901..2000])
