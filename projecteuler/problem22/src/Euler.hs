637a7306a801fa1363788bdeb4df5da3781fa9cf
Hyunje Alex Jun <me@noraesae.net>
2015-11-23 20:22:57 +0900

    Write test cases for problem22

--- projecteuler/problem22/src/Euler.hs
+++ projecteuler/problem22/src/Euler.hs
@@ -1,3 +1,15 @@
 module Euler
-(
+( parseNames
+, names
 ) where
+
+import System.IO
+
+parseNames :: String -> [String]
+parseNames str = []
+
+names :: [String]
+names = do
+  {- contents <- readFile "resource/names.txt" ReadMode-}
+  {- splitAndParse contents-}
+  []
--- projecteuler/problem22/test/Spec.hs
+++ projecteuler/problem22/test/Spec.hs
@@ -3,6 +3,15 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "" $ do
-    it "" $ do
-      1 `shouldBe` 1
+  describe "parseNames" $ do
+    it "parses name list string" $ do
+      (parseNames "\"MARY\",\"PATRICIA\",\"LINDA\"") `shouldMatchList` ["MARY", "PATRICIA", "LINDA"]
+
+  describe "names" $ do
+    it "returns names" $ do
+      names `shouldContain` ["MARY"]
+      names `shouldContain` ["BARBARA"]
+      names `shouldContain` ["SUSAN"]
+      names `shouldContain` ["LISA"]
+      names `shouldContain` ["NANCY"]
+      (length names) `shouldBe` 5000
