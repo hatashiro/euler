e7ce1ce576e8701f559439f404dc293c6ce319c2
Hyunje Alex Jun <me@noraesae.net>
2015-12-21 23:59:57 +0900

    Solve Project Euler #25

--- projecteuler/problem25/src/Euler.hs
+++ projecteuler/problem25/src/Euler.hs
@@ -1,6 +1,27 @@
 module Euler
-    ( someFunc
-    ) where
+( fibo
+, memoFibo
+, digit
+, firstNDigitFibo
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+fibo :: Int -> Integer
+fibo 1 = 1
+fibo 2 = 1
+fibo n = (memoFibo (n-1)) + (memoFibo (n-2))
+
+memoFibo :: Int -> Integer
+memoFibo = ((map fibo [0..]) !!)
+
+digit :: Integer -> Int
+digit 0 = 0
+digit n = 1 + (digit (n `div` 10))
+
+firstNDigitFibo :: Int -> Int
+firstNDigitFibo n = firstNDigitFiboWithIdx 1 n
+
+firstNDigitFiboWithIdx :: Int -> Int -> Int
+firstNDigitFiboWithIdx idx targetDigit =
+  if (digit.memoFibo $ idx) == targetDigit
+    then idx
+    else firstNDigitFiboWithIdx (idx + 1) targetDigit
--- projecteuler/problem25/test/Spec.hs
+++ projecteuler/problem25/test/Spec.hs
@@ -3,6 +3,46 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "" $ do
-    it "" $ do
-      1 `shouldBe` 1
+  describe "fibo" $ do
+    it "calculates a finonacci number" $ do
+      (fibo 1) `shouldBe` 1
+      (fibo 2) `shouldBe` 1
+      (fibo 3) `shouldBe` 2
+      (fibo 4) `shouldBe` 3
+      (fibo 5) `shouldBe` 5
+      (fibo 6) `shouldBe` 8
+      (fibo 7) `shouldBe` 13
+      (fibo 8) `shouldBe` 21
+      (fibo 9) `shouldBe` 34
+      (fibo 10) `shouldBe` 55
+      (fibo 11) `shouldBe` 89
+      (fibo 12) `shouldBe` 144
+
+  describe "memoFibo" $ do
+    it "is memoized fibo" $ do
+      (memoFibo 1) `shouldBe` 1
+      (memoFibo 2) `shouldBe` 1
+      (memoFibo 3) `shouldBe` 2
+      (memoFibo 4) `shouldBe` 3
+      (memoFibo 5) `shouldBe` 5
+      (memoFibo 6) `shouldBe` 8
+      (memoFibo 7) `shouldBe` 13
+      (memoFibo 8) `shouldBe` 21
+      (memoFibo 9) `shouldBe` 34
+      (memoFibo 10) `shouldBe` 55
+      (memoFibo 11) `shouldBe` 89
+      (memoFibo 12) `shouldBe` 144
+
+  describe "digit" $ do
+    it "gets the number of digits from an integer" $ do
+      (digit 3) `shouldBe` 1
+      (digit 10) `shouldBe` 2
+      (digit 67) `shouldBe` 2
+      (digit 100) `shouldBe` 3
+      (digit 1000) `shouldBe` 4
+      (digit 1591) `shouldBe` 4
+
+  describe "firstNDigitFibo" $ do
+    it "gets a fibo seq having n digit for the 1st time" $ do
+      (firstNDigitFibo 2) `shouldBe` 7
+      (firstNDigitFibo 3) `shouldBe` 12
