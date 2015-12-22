30ba7007d900285e7a6dea7831a73dcaf5455d38
Hyunje Alex Jun <me@noraesae.net>
2015-12-22 23:51:38 +0900

    Solve Project Euler #26

--- projecteuler/problem26/app/Main.hs
+++ projecteuler/problem26/app/Main.hs
@@ -3,4 +3,8 @@ module Main where
 import Euler
 
 main :: IO ()
-main = putStrLn "hello"
+main = putStrLn.show $ foldl (\maxi n ->
+  let len = (length $ getRecurringCycle n) in
+  if (snd maxi) < len
+    then (n, len)
+    else maxi) (0, 0) [1..1000]
