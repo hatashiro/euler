324149e04a79e8237987455385ff6dd9e8a746b0
Hyunje Alex Jun <me@noraesae.net>
2015-12-26 22:51:49 +0900

    Solve Project Euler #29

--- projecteuler/problem29/app/Main.hs
+++ projecteuler/problem29/app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import Euler
+import Data.List
+
+rmdups :: (Ord a) => [a] -> [a]
+rmdups = map head . group . sort
 
 main :: IO ()
-main = putStrLn "hello"
+main = putStrLn.show $ length $ rmdups [a ^ b | a <- [2..100], b <- [2..100]]
