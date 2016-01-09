8cb887e3058a08aa9f3368c5f525607c816b5807
Hyunje Alex Jun <me@noraesae.net>
2016-01-09 23:47:17 +0900

    Try memoization in factorial

--- projecteuler/problem34/src/Euler.hs
+++ projecteuler/problem34/src/Euler.hs
@@ -3,8 +3,10 @@ module Euler where
 import Data.Char
 
 factorial :: Int -> Int
-factorial 0 = 1
-factorial n = n * factorial (n-1)
+factorial = (map fac [0..] !!)
+  where
+    fac 0 = 1
+    fac n = n * factorial (n-1)
 
 digits :: Int -> [Int]
 digits n = map digitToInt $ show n
