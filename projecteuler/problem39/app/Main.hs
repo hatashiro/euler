cc2eae4172ed4dc6001f43cde10b9dda4cab3ea5
Hyunje Alex Jun <me@noraesae.net>
2016-01-16 18:33:18 +0900

    Solve Project Euler #39

--- projecteuler/problem39/app/Main.hs
+++ projecteuler/problem39/app/Main.hs
@@ -2,5 +2,8 @@ module Main where
 
 import Euler
 
+maxN :: (Ord a, Ord b) => (a -> b) -> [a] -> a
+maxN f xs = snd . maximum $ zip (map f xs) xs
+
 main :: IO ()
-main = putStrLn "hello"
+main = print $ maxN (length.pythagorianTriples) [3..1000]
--- projecteuler/problem39/test/Spec.hs
+++ projecteuler/problem39/test/Spec.hs
@@ -2,7 +2,15 @@ import Euler
 import Test.Hspec
 
 main :: IO ()
-main = hspec $
-  describe "" $
-    it "" $
-      1 `shouldBe` 1
+main = hspec $ do
+  describe "isPythagoreanTriples" $
+    it "tells if three integers form a pythagorian triple" $ do
+      isPythagoreanTriples 1 2 3 `shouldBe` False
+      isPythagoreanTriples 3 4 5 `shouldBe` True
+      isPythagoreanTriples 20 48 52 `shouldBe` True
+      isPythagoreanTriples 24 45 51 `shouldBe` True
+      isPythagoreanTriples 30 40 50 `shouldBe` True
+
+  describe "pythagorianTriples" $
+    it "returns a list of pythagorian triples a sum of which is an integer" $
+      pythagorianTriples 120 `shouldMatchList` [(20,48,52), (24,45,51), (30,40,50)]
