8f50cdf8572df842783dbd730269bdd54447e95b
Hyunje Alex Jun <me@noraesae.net>
2016-01-17 23:19:26 +0900

    Solve Project Euler #43

--- projecteuler/problem43/app/Main.hs
+++ projecteuler/problem43/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
+import Data.Digits
 import Euler
 
 main :: IO ()
-main = putStrLn "hello"
+main = print.sum $ map (unDigits 10) (filter isSubstringDivisible pandigitals)
--- projecteuler/problem43/test/Spec.hs
+++ projecteuler/problem43/test/Spec.hs
@@ -2,7 +2,19 @@ import Euler
 import Test.Hspec
 
 main :: IO ()
-main = hspec $
-  describe "" $
-    it "" $
-      1 `shouldBe` 1
+main = hspec $ do
+  describe "pandigitals" $
+    it "returns a list of 0-9 pandigital numbers" $ do
+      pandigitals `shouldContain` [[1,2,3,4,5,6,7,8,9,0]]
+      pandigitals `shouldContain` [[2,3,5,1,4,6,9,7,0,8]]
+      pandigitals `shouldNotContain` [[0,1,2,3,4,5,6,7,8,9]]
+
+  describe "sublist" $
+    it "returns a sublist of a list" $ do
+      sublist 0 2 [1, 2, 3] `shouldBe` [1, 2]
+      sublist 3 3 [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] `shouldBe` [4, 5, 6]
+
+  describe "isSubstringDivisible" $
+    it "tells if an integer is sub-string divisible" $ do
+      isSubstringDivisible [1,4,0,6,3,5,7,2,8,9] `shouldBe` True
+      isSubstringDivisible [1,4,0,6,3,5,7,2,8,8] `shouldBe` False
