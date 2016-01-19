b546903cce1926bf8cd03be51d0e2e819b1bc910
Hyunje Alex Jun <me@noraesae.net>
2016-01-19 17:42:58 +0900

    Enhance problem44 using the perfect square test
    https://en.wikipedia.org/wiki/Pentagonal_number#The_perfect_square_test

--- projecteuler/problem44/src/Euler.hs
+++ projecteuler/problem44/src/Euler.hs
@@ -3,11 +3,22 @@ module Euler where
 pentagonNumber :: Int -> Int
 pentagonNumber n = n * (3*n - 1) `div` 2
 
-isPentagonNumber :: Int -> Bool
-isPentagonNumber n = isPentagonNumber' n 1
+isqrt :: Int -> Maybe Int
+isqrt n = binarySearch 1 n
   where
-    isPentagonNumber' n i = n == ith || (ith < n && isPentagonNumber' n (i+1))
-      where ith = pentagonNumber i
+    binarySearch low high =
+      let mid = (high + low) `div` 2
+          midSquare = mid * mid in
+      if low > high then Nothing
+      else if n == midSquare then Just mid
+      else if n < midSquare then binarySearch low (mid-1)
+      else binarySearch (mid+1) high
+
+isPentagonNumber :: Int -> Bool
+isPentagonNumber p =
+  case isqrt (1 + 24*p) of
+    Just n  -> n `mod` 6 == 5
+    Nothing -> False
 
 isSumPentagon :: (Int, Int) -> Bool
 isSumPentagon (a, b) = isPentagonNumber (aPen + bPen)
--- projecteuler/problem44/test/Spec.hs
+++ projecteuler/problem44/test/Spec.hs
@@ -7,6 +7,16 @@ main = hspec $ do
     it "returns a pentagon number for an index" $
       map pentagonNumber [1..10] `shouldBe` [1, 5, 12, 22, 35, 51, 70, 92, 117, 145]
 
+  describe "isqrt" $
+    it "returns an integer sqrt if exists" $ do
+      isqrt 1 `shouldBe` Just 1
+      isqrt 2 `shouldBe` Nothing
+      isqrt 4 `shouldBe` Just 2
+      isqrt 15 `shouldBe` Nothing
+      isqrt 16 `shouldBe` Just 4
+      isqrt 23 `shouldBe` Nothing
+      isqrt 25 `shouldBe` Just 5
+
   describe "isPentagonNumber" $
     it "tells if a number is a pentagon number" $ do
       isPentagonNumber 51 `shouldBe` True
@@ -18,7 +28,3 @@ main = hspec $ do
   describe "isSumPentagon" $
     it "pairs forming a pentagon number by sum of two pentagon numbers" $
       isSumPentagon (4, 7) `shouldBe` True
-
-  describe "sumPentagonPairs" $
-    it "returns a list of sum pentagon pairs" $
-      sumPentagonPairs `shouldContain` [(7, 4)]
