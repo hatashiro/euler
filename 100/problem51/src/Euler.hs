072953f4b12efbbdbf9e415b5e0f0b7badd364d4
Hyunje Alex Jun <me@noraesae.net>
2016-01-25 23:52:21 +0900

    Implement needed functions for problem51

--- 100/problem51/src/Euler.hs
+++ 100/problem51/src/Euler.hs
@@ -1,6 +1,23 @@
 module Euler where
 
-isPrime :: Integer -> Bool
+import Data.Digits
+import Data.List
+
+isPrime :: Int -> Bool
 isPrime n = not (any divisible [2..r])
   where r = floor (sqrt (fromIntegral n))
         divisible m = n `mod` m == 0
+
+primes :: [Int]
+primes = filter isPrime [2..]
+
+findDigitPositions :: Int -> Int -> [Int]
+findDigitPositions n d = elemIndices d (digits 10 n)
+
+replaceDigitPositions :: Int -> [Int] -> Int -> Int
+replaceDigitPositions n indices d = unDigits 10 $
+  helper (digits 10 n) indices d
+    where
+      helper ds []     _ = ds
+      helper ds (i:is) d =
+        take i ds ++ d:helper (drop (i+1) ds) (map (subtract (i+1)) is) d
--- 100/problem51/test/Spec.hs
+++ 100/problem51/test/Spec.hs
@@ -2,7 +2,14 @@ import Euler
 import Test.Hspec
 
 main :: IO ()
-main = hspec $
-  describe "" $
-    it "" $
-      1 `shouldBe` 1
+main = hspec $ do
+  describe "findDigitPositions" $
+    it "finds digit positions of a digit from an integer" $ do
+      findDigitPositions 15134 1 `shouldBe` [0, 2]
+      findDigitPositions 15134 5 `shouldBe` [1]
+      findDigitPositions 15134 0 `shouldBe` []
+
+  describe "replaceDigitPositions" $
+    it "replaces digits in the positions with another digit" $ do
+      replaceDigitPositions 15134 [0, 2] 3 `shouldBe` 35334
+      replaceDigitPositions 15134 [1] 9 `shouldBe` 19134
