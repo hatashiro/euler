1a8ce2f5fcc6c7149ec911772a8b0ca376179646
Hyunje Alex Jun <me@noraesae.net>
2016-01-30 23:58:53 +0900

    wip problem55

--- 100/problem55/app/Main.hs
+++ 100/problem55/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Data.Maybe
 import Euler
 
 main :: IO ()
-main = putStrLn "hello"
+main = print $
+  length $ filter isNothing (map itersForPalindrome [1..10000])
--- 100/problem55/src/Euler.hs
+++ 100/problem55/src/Euler.hs
@@ -1,1 +1,21 @@
 module Euler where
+
+import Data.Digits
+
+isPalindrome :: Integer -> Bool
+isPalindrome n = isPalindrome' (digits 10 n) []
+  where isPalindrome' xs@(x':xs') ys
+          | length xs == length ys = xs == ys
+          | length xs <  length ys = xs == tail ys
+          | otherwise              = isPalindrome' xs' (x':ys)
+
+reverseAdd :: Integer -> Integer
+reverseAdd n = n + (unDigits 10.reverse.digits 10) n
+
+itersForPalindrome :: Integer -> Maybe Int
+itersForPalindrome n = itersForPalindrome' (reverseAdd n) 1
+  where
+    itersForPalindrome' result iters
+      | iters > 50          = Nothing
+      | isPalindrome result = Just iters
+      | otherwise           = itersForPalindrome' (reverseAdd result) (iters+1)
