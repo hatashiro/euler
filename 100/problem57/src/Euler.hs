4dfdc02b830a1c14211d22e133bc09de3e52ff46
Hyunje Alex Jun <me@noraesae.net>
2016-02-01 17:46:27 +0900

    Use Integer instead of Integral
    It makes the calculation much faster.

--- 100/problem57/src/Euler.hs
+++ 100/problem57/src/Euler.hs
@@ -3,9 +3,9 @@ module Euler where
 import Data.Digits
 import Data.Ratio
 
-data Fraction a = Fraction a a deriving (Eq, Show)
+data Fraction = Fraction Integer Integer deriving (Eq, Show)
 
-instance (Integral a) => Num (Fraction a) where
+instance Num Fraction where
   Fraction a b + Fraction c d = normalize (Fraction (a*d + b*c) (b*d))
   Fraction a b * Fraction c d = normalize (Fraction (a*c) (b*d))
   abs (Fraction a b) = Fraction (abs a) b
@@ -13,14 +13,14 @@ instance (Integral a) => Num (Fraction a) where
   fromInteger i = Fraction (fromInteger i) 1
   negate (Fraction a b) = Fraction (-a) b
 
-instance (Integral a) => Fractional (Fraction a) where
+instance Fractional Fraction where
   recip (Fraction a b) = normalize (Fraction b a)
   fromRational r = normalize (Fraction (fromIntegral $ numerator r) (fromIntegral $ denominator r))
 
-toFloat :: (Integral a) => Fraction a -> Float
+toFloat :: Fraction -> Float
 toFloat (Fraction a b) = fromIntegral a / fromIntegral b
 
-normalize :: (Integral a) => Fraction a -> Fraction a
+normalize :: Fraction -> Fraction
 normalize (Fraction a b) = Fraction (sig*numerator) denominator
   where
     sig = signum a * signum b
@@ -29,11 +29,11 @@ normalize (Fraction a b) = Fraction (sig*numerator) denominator
     numerator   = absA `div` gcd absA absB
     denominator = absB `div` gcd absA absB
 
-sqrtIteration :: (Integral a) => Int -> Fraction a
+sqrtIteration :: Int -> Fraction
 sqrtIteration = (map sqrtIteration' [0..] !!)
   where
     sqrtIteration' 1 = Fraction 3 2
     sqrtIteration' n = 1 + 1 / (1 + sqrtIteration (n-1))
 
-hasLongerNumerator :: (Integral a) => Fraction a -> Bool
+hasLongerNumerator :: Fraction -> Bool
 hasLongerNumerator (Fraction a b) = length (digits 10 a) > length (digits 10 b)
