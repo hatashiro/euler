5aba0ac8c6cef2154caea4fd82d9983b7a67b099
Hyunje Alex Jun <me@noraesae.net>
2016-02-05 17:02:36 +0900

    Solve 59

--- 100/problem59/app/Main.hs
+++ 100/problem59/app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Data.Char
+import Data.List
+import Data.Maybe
 import Euler
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  raw <- readFile "p059_cipher.txt"
+  let codes = parseIntList raw
+  print $
+    case find isJust $ map (decrypt codes) possiblePasswords of
+      Just (Just str) -> sum (map ord str)
+      Nothing  -> -1
