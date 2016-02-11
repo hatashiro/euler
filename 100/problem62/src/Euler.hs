6d05e3ee340790c88ece93214ae19ab422194a0e
Hyunje Alex Jun <me@noraesae.net>
2016-02-11 23:23:57 +0900

    Solve 62

--- 100/problem62/src/Euler.hs
+++ 100/problem62/src/Euler.hs
@@ -1,1 +1,20 @@
 module Euler where
+
+import qualified Data.HashMap as M
+import Data.List
+
+cubes :: [Int]
+cubes = map (^3) [1..]
+
+getHashKey :: Int -> String
+getHashKey n = sort (show n)
+
+cubicPermutations :: Int -> Int
+cubicPermutations n = cubicPermutations' n M.empty cubes
+  where
+    cubicPermutations' n hashMap (cube:cubes) =
+      let key = getHashKey cube
+          list = cube : M.findWithDefault [] key hashMap in
+      if length list == n
+         then minimum list
+         else cubicPermutations' n (M.insert key list hashMap) cubes
--- 100/problem62/test/Spec.hs
+++ 100/problem62/test/Spec.hs
@@ -2,7 +2,10 @@ import Euler
 import Test.Hspec
 
 main :: IO ()
-main = hspec $
-  describe "" $
-    it "" $
-      1 `shouldBe` 1
+main = hspec $ do
+  describe "getHashKey" $
+    it "returns a hash key for a cubic integer" $
+      getHashKey 41063625 `shouldBe` "01234566"
+  describe "cubicPermutations" $
+    it "returns the smallest cube has n cubic permutations" $
+      cubicPermutations 3 `shouldBe` 41063625
