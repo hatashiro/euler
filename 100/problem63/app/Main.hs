a393f9ed74476fd571e8df674d0df05147fc036b
Hyunje Alex Jun <me@noraesae.net>
2016-02-12 21:32:10 +0900

    Solve 63

--- 100/problem63/app/Main.hs
+++ 100/problem63/app/Main.hs
@@ -3,4 +3,5 @@ module Main where
 import Euler
 
 main :: IO ()
-main = putStrLn "hello"
+main = print $
+  sum $ takeWhile (>0) $ map (length.nDigitNthPowers) [1..]
--- 100/problem63/test/Spec.hs
+++ 100/problem63/test/Spec.hs
@@ -3,6 +3,7 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $
-  describe "" $
-    it "" $
-      1 `shouldBe` 1
+  describe "nDigitNthPowers" $
+    it "returns a list of nth powers with n digits" $ do
+      nDigitNthPowers 1 `shouldBe` [1..9]
+      nDigitNthPowers 2 `shouldBe` [16, 25, 36, 49, 64, 81]
