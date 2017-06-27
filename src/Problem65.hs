2c481688ce00f017774bd001000b842120a3c170
Hyunje Jun <me@noraesae.net>
2017-06-27 16:29:48 +0900

    Solve problem 65

--- src/Problem65.hs
+++ src/Problem65.hs
@@ -1,4 +1,28 @@
 module Problem65 where
 
+import Data.Ratio
+
 main :: IO ()
-main = putStrLn "hello"
+main = print $ digitSum (numerator $ e 100)
+
+fk :: Int -> Integer
+fk x = case x `mod` 3 of
+         2 -> 2 * fromIntegral (x `div` 3 + 1)
+         _ -> 1
+
+fractions :: [Integer]
+fractions = map fk [1..]
+
+inverse :: Rational -> Rational
+inverse x = denominator x % numerator x
+
+e :: Int -> Rational
+e acc = 2 + go (take (acc - 1) fractions)
+  where
+  go :: [Integer] -> Rational
+  go [] = 0
+  go [i] = 1 % i
+  go (i:is) = inverse (fromIntegral i + go is)
+
+digitSum :: Integer -> Integer
+digitSum x = sum $ map (read . (:[])) (show x)
